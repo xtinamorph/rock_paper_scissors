c6e700c27a1567dd50c6f409bd9c17ad2a257e26
xtinamorph <250262704+xtinamorph@users.noreply.github.com>
2026-04-19 17:42:26 -0500

    create functions for computer and human imput
    created to functions that generate one of three answers: rock, paper, or
    scissors. Two variables were also created to hold both of these answers.

--- javascript.js
+++ javascript.js
@@ -1,1 +1,29 @@
 console.log("Hello World!")
+function getComputerChoice() {
+    let num = Math.floor(Math.random() * 3); 
+    if (num === 0) {
+        return 'Rock'; 
+    } else if (num === 1) {
+        return 'Paper'; 
+    } else {
+        return 'Scissors'
+    }
+ }
+
+console.log(getComputerChoice()) 
+function getHumanChoice() {
+    let choice = prompt("Rock, Paper, or Scissors?");
+    if (choice === "Rock") {
+        return 'Rock' 
+    } 
+    else if (choice === 'Paper') {
+        return 'Paper'
+    }
+    else {
+        return 'Scissors'
+    }
+} 
+console.log(getHumanChoice())
+
+const humanScore = 0
+const computerScore = 0
