be5b243793bbfc68556db900ace89f78191ff53c
xtinamorph <250262704+xtinamorph@users.noreply.github.com>
2026-05-01 21:16:07 -0500

    Create function to play one round of the game
    A function was created so that one round of rock papper scissors will be
    played. The next step will be to make this process repeat for 5 rounds.

--- javascript.js
+++ javascript.js
@@ -1,9 +1,9 @@
 console.log("Hello World!")
 function getComputerChoice() {
-    let num = Math.floor(Math.random() * 3); 
-    if (num === 0) {
+    let computerChoice = Math.floor(Math.random() * 3); 
+    if (computerChoice === 0) {
         return 'rock'; 
-    } else if (num === 1) {
+    } else if (computerChoice === 1) {
         return 'paper'; 
     } else {
         return 'scissors'
@@ -12,38 +12,44 @@ function getComputerChoice() {
 
 console.log(getComputerChoice()) 
 function getHumanChoice() {
-    let choice = prompt("Rock, Paper, or Scissors?");
-    if (choice == "rock") {
+    let humanChoice = prompt("Rock, Paper, or Scissors?");
+    humanChoice = humanChoice.toLowerCase();
+    if (humanChoice == "rock") {
         return 'rock' 
     } 
-    else if (choice == 'paper') {
+    else if (humanChoice == 'paper') {
         return 'paper'
     }
     else {
         return 'scissors'
     }
 } 
-console.log(getHumanChoice())
+
 
 let humanScore = 0
 let computerScore = 0 
-
 function playRound(humanChoice, computerChoice) {
-humanChoice = humanChoice.toLowerCase(); 
 if (
     (humanChoice === 'rock' && computerChoice === 'scissors') || 
     (humanChoice === 'paper' && computerChoice === 'rock') || 
     (humanChoice === 'scissors' && computerChoice === 'paper') 
-) {return console.log(`You Win! ${humanChoice} beats ${computerChoice}.`); 
+) { ++humanScore
+    return console.log(`You Win! ${humanChoice} beats ${computerChoice}.`); 
 }else if (humanChoice === computerChoice) {
     return console.log("It's a tie!"); 
- } else {
+ } else { 
+        ++computerScore
         return console.log(`You lose! ${computerChoice} beats ${humanChoice}.`); 
     } 
 }
 
-
 const humanSelection = getHumanChoice() 
 const computerSelection = getComputerChoice() 
+function playGame() {
+playRound(humanSelection, computerSelection) 
+}
 
-playRound(humanSelection, computerSelection)
+playGame();
+
+console.log(humanScore);
+console.log(computerScore);
